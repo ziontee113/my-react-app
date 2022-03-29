4fadb3422b62532bd6ab4fb664c55176c379a5ad
Dev Masta <new@mail.com>
2022-03-29 23:11:45 +0700

    added simple increment buttons

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const decrementCount = () => setCount((prevCount) => prevCount - 1);
-  const incrementCount = () => setCount((prevCount) => prevCount + 1);
+  const [increment, setIncrement] = useState(1);
+
+  const decrementCount = () => setCount((prevCount) => prevCount - increment);
+  const incrementCount = () => setCount((prevCount) => prevCount + increment);
+
   const changeClass = () =>
     count > 0 ? "positive" : count < 0 ? "negative" : null;
 
+  const incrementMore = () => setIncrement((prevCount) => prevCount + 1);
+  const incrementLess = () => setIncrement((prevCount) => prevCount - 1);
+
   return (
     <div className="App">
-      <h1 className={changeClass()}>{count}</h1>
+      <h1 className={changeClass() + " count"}>{count}</h1>
+      <h1 className="increment">{increment}</h1>
+      <div className="button-wrapper increment-button-wrapper">
+        <button onClick={incrementLess}>-</button>
+        <button onClick={incrementMore}>+</button>
+      </div>
       <div className="button-wrapper">
         <button onClick={decrementCount}>-</button>
         <button onClick={incrementCount}>+</button>
