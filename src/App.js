669607f47ba0d4a74d64ddb08438d1b7a1335508
Dev Masta <new@mail.com>
2022-03-29 17:43:19 +0700

    finished, need to learn what we did

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function App() {
   const [count, setCount] = useState(0);
   const decrementCount = () => setCount((prevCount) => prevCount - 1);
   const incrementCount = () => setCount((prevCount) => prevCount + 1);
+  const changeClass = () =>
+    count > 0 ? "positive" : count < 0 ? "negative" : null;
 
   return (
     <div className="App">
-      <h1>{count}</h1>
+      <h1 className={changeClass()}>{count}</h1>
       <div className="button-wrapper">
         <button onClick={decrementCount}>-</button>
         <button onClick={incrementCount}>+</button>
